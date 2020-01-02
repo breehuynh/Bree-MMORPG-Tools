67c581c8aad4662b3dadbcb4242ca6ec22e507de
Bree Huynh <breehuynh@brees-imac.attlocal.net>
2020-01-01 18:14:23 -0800

    reorganized folder structure

---
+++ ! Boba Utils (REQUIRED) !/README.md
@@ -0,0 +1,11 @@
+## Boba Utils
+
+*Made by bobatea#9400 on Discord!*
+
+### DESCRIPTION:
+
+- Utilities and scripts for all addons to work
+
+### INSTRUCTIONS: 
+
+1. Drag and drop the files to your project
---
+++ ! Boba Utils (REQUIRED) !/Scripts/EntityPartial.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public partial class Entity
+{
+    // Sets new target
+    public void PartialOnAggro(Entity entity)
+    {
+        target = entity;
+        OnAggro(target);
+    }
+}
